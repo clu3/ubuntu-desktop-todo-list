11f9a9245e0a10a49a86eb07c251a948f7648b31
Steve <steve.tran.38@gmail.com>
2011-08-17 08:24:35 +0700

    Fixed task group heading

--- insert_text.php
+++ insert_text.php
@@ -1,28 +1,33 @@
 <?php
 /** Configurable */
 $file = 'todo.txt'; //todo list . each line a task
-$bgImage = "/home/tran/Pictures/wallpapers/bg.jpg";
+$bgImage = "/home/tran/Pictures/wallpapers/bg2.jpg";
 $activeBgImage = "/home/tran/Pictures/wallpapers/active.jpg";
 $fontSize = 20;
 $x = 100; //offset
 $y = 650; //offset
-$listTitle = "My TODO list";
+
 $fontFile = './fonts/FreeMonoBold.ttf';
 
 //----------start
 $im = imagecreatefromjpeg ( $bgImage );
 $textColor = imagecolorallocate($im, 0,0,0); //black
 
-$i = 1;
-imagefttext($im, $fontSize, 0, $x, $y, $textColor, $fontFile, $listTitle);
+$i = 1; //line number
 //write each line 
 $handle = @fopen($file, "r");
 $lineHeight = $fontSize * 2;
 if ($handle) {
     while (($buffer = fgets($handle, 4096)) !== false) {
         $y = $y + $lineHeight;
-        imagefttext($im, $fontSize, 0, $x, $y, $textColor, $fontFile,"#$i. " . $buffer);
-        $i++;
+        if ($buffer != strtoupper($buffer) && trim($buffer) != '')
+        {
+            imagefttext($im, $fontSize, 0, $x, $y, $textColor, $fontFile,"#$i. " . $buffer);
+            $i++;
+        }
+        else 
+            imagefttext($im, $fontSize, 0, $x, $y, $textColor, $fontFile, $buffer);
+
     }
     if (!feof($handle)) {
         echo "Error: unexpected fgets() fail\n";
